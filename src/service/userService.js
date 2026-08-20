bbeec25d96f3f557bd94c2bd5cb27746a1610855
Dev User <applelookbym@gmail.com>
2026-08-20 14:32:07 +0530

    DB Changes

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = process.env.VITE_API_URL;
+
+const API_URL = import.meta.env.VITE_API_URL;
 export const loginUser = async (email, password) => {
   try {
     const token = localStorage.getItem("token");
